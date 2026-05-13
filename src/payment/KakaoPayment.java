c5921da6c6701b971d01efb9b9d1024113dcf145
6AM8NAB3 <junhojo749@gmail.com>
2026-05-13 17:13:37 +0900

    추상클래스 공부

---
+++ src/payment/KakaoPayment.java
@@ -0,0 +1,14 @@
+package payment;
+
+public class KakaoPayment extends Payment{
+    public String userid;
+
+    public KakaoPayment(int amount, String userid) {
+        super(amount);
+        this.userid = userid;
+    }
+
+    public void processPay() {
+        System.out.println("카드 결제 진행 중.... [유저 id: "+userid+"] [금액: "+amount+"]");
+    }
+}
--- src/payment/PaymentMain.java
+++ src/payment/PaymentMain.java
@@ -2,11 +2,14 @@ package payment;
 
 public class PaymentMain {
     public  static void main(String[] args) {
-        Payment payments = new CardPayment(10000,"xxxx-xxxx");
-
-        payments.processPay();
-        payments.showBasicInfo();
-        
+        Payment[] payments  = {
+                new CardPayment(30000,"2708-5914"),
+                new KakaoPayment(30000,"6AM8NAB3")
+        };
+        for (Payment p: payments) {
+            p.processPay();
+            p.showBasicInfo();
         }
     }
+}
 
