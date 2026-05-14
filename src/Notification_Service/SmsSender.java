3a044d8431056cd5302a96828691e44bd6ca8351
6AM8NAB3 <junhojo749@gmail.com>
2026-05-14 20:25:27 +0900

    인터페리스 복습

---
+++ src/Notification_Service/SmsSender.java
@@ -0,0 +1,14 @@
+package Notification_Service;
+
+public class SmsSender implements MessageSender{
+    private String message;
+
+    public SmsSender(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public void sendMessage(String message) {
+        System.out.println("sms 문자 메세지 전송중... / 내용: "+message);
+    }
+}
